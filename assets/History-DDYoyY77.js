bd76b39112200fa08afce332458fa2385f591512
HFC06Atyrau <Lobikeane@yandex.ru>
2026-01-09 20:52:00 +0500

    Updates

--- assets/History-DDYoyY77.js
+++ assets/History-DDYoyY77.js
@@ -1,4 +1,4 @@
-import{c as I,s as g,r as p,j as e,u as C,B as P}from"./index-ZT2MSVJ0.js";import{k,g as _,u as M,H as A,q as E,d as B,f as F}from"./avatar-CFtkXPdP.js";import{u as H,b as q,g as L}from"./usePlayerStats-wxlRwWTE.js";import{u as R}from"./useTourTeams-BrBX9VCv.js";import{S as w,a as v,b as S,c as T,d as D,j as N,k as V,D as y,g as b,i as $}from"./DreamTeamSelector-DfThWKpf.js";import"./trophy-CEsnfYY3.js";import"./index-5-ooDDT8.js";/**
+import{c as I,s as g,r as p,j as e,u as C,B as P}from"./index-CTDFMwvo.js";import{k,g as _,u as M,H as A,q as E,d as B,f as F}from"./avatar-loYbL3fM.js";import{u as H,b as q,g as L}from"./usePlayerStats-q0TMpflh.js";import{u as R}from"./useTourTeams-2SXHuv4t.js";import{S as w,a as v,b as S,c as T,d as D,j as N,k as V,D as y,g as b,i as $}from"./DreamTeamSelector-DuzH8fdN.js";import"./trophy-BMYLibfw.js";import"./index-C8u8Bqvc.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
---
+++ assets/useTourDreamTeamsBySeason-CggsFnLp.js
@@ -0,0 +1,6 @@
+import{c as i,s as o}from"./index-CTDFMwvo.js";import{k as m}from"./avatar-loYbL3fM.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=i("Target",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"6",key:"1vlfrh"}],["circle",{cx:"12",cy:"12",r:"2",key:"1c9p78"}]]);function d(r){return m({queryKey:["tour-dream-teams-by-season",r],queryFn:async()=>{if(!r)return[];const{data:e,error:t}=await o.from("tours").select("id").eq("season_id",r);if(t)throw t;if(!e||e.length===0)return[];const c=e.map(u=>u.id),{data:s,error:a}=await o.from("tour_dream_teams").select("*").in("tour_id",c).eq("team_type","dream");if(a)throw a;return s},enabled:!!r})}export{f as T,d as u};
